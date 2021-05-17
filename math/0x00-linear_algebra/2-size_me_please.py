59bd9ec69bb8e101c671dff86c0ed03bcfa7de92
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2021-05-17 00:11:41 -0500

    [Add] Recursive to get matrix shape

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-def get_len(lst, shape):
+def get_shape(lst, shape):
+    """
+    Get the len recursively in a matrix
+    :param lst: The first element en the previous list
+    :param shape: List with save the len in each dimension
+    :return: A list with the len of each dimension
+    """
     if isinstance(lst, int):
         return None
 
     shape.append(len(lst))
-    get_len(lst[0], shape)
+    get_shape(lst[0], shape)
     return shape
 
 
 def matrix_shape(matrix):
-    return get_len(matrix, [])
+    """
+    Call the recursive function
+    :param matrix: Matrix to get the shape
+    :return: A list with the matrix shape
+    """
+    return get_shape(matrix, [])
