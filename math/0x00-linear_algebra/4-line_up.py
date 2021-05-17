e1bf5acfc6e886ba59ec1e1cbe957352ecede468
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2021-05-17 00:54:10 -0500

    [Add] Summ 2 arrays

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Add 2 arrays 1D"""
+
+
+def get_shape(lst, shape):
+    """
+    Get the len recursively in a matrix
+    :param lst: The first element en the previous list
+    :param shape: List with save the len in each dimension
+    :return: A list with the len of each dimension
+    """
+    if isinstance(lst, int) or isinstance(lst, float):
+        return None
+
+    shape.append(len(lst))
+    get_shape(lst[0], shape)
+    return shape
+
+
+def matrix_shape(matrix):
+    """
+    Call the recursive function
+    :param matrix: Matrix to get the shape
+    :return: A list with the matrix shape
+    """
+    return get_shape(matrix, [])
+
+
+def add_arrays(arr1, arr2):
+    """
+    Sum 2 arrays. 1d
+    :param arr1: Array 1
+    :param arr2: Array 2
+    :return: An array sum
+    """
+    if matrix_shape(arr1) != matrix_shape(arr2):
+        return None
+
+    final = []
+    for i in range(len(arr1)):
+        res = arr1[i] + arr2[i]
+        final.append(res)
+
+    return final
