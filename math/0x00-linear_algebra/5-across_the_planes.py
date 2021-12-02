836f0d146a5d8737d99cea6840ac4443f32a9447
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2021-12-02 13:50:50 -0500

    add 5 excersice

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Add 2 arrays 1D"""
+"""Add 2 arrays 2D"""
 
 
 def add_arrays(arr1, arr2):
@@ -19,5 +19,25 @@ def add_arrays(arr1, arr2):
     for i in range(len(arr1)):
         res = arr1[i] + arr2[i]
         final.append(res)
+
+    return final
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Sum 2 arrays. 2d
+    :param arr1: Array 1
+    :param arr2: Array 2
+    :return: An array sum
+    """
+    if len(mat1) != len(mat2):
+        return None
+
+    final = []
+    for i in range(len(mat1)):
+        final.append(add_arrays(mat1[i], mat2[i]))
+
+    if None in final:
+        return None
 
     return final
